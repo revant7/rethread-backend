a9ab36742f9df9f6e1304f359d718b217c55f1b6
Revant <86226975+revant7@users.noreply.github.com>
2025-04-02 23:26:41 +0530

    Updated API Documentation And Redoc

--- rethread/urls.py
+++ rethread/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("home.urls")),
+    path("", include("home.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
